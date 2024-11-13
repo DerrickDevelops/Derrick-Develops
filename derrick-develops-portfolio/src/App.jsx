782bf9ab9d7677ebe2ed60b57370ea02b7749366
DerrickDevelops <derrickdevelops@gmail.com>
2024-11-13 18:58:05 -0500

    Added New Files
    Added Footer element & added App file to transfer elements to.

---
+++ derrick-develops-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import Header from './Header.jsx'
+// import Content from './Content.jsx'
+// import Footer from './Footer.jsx'
+import './layout.css';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
---
+++ derrick-develops-portfolio/src/Footer.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+import './Layout.css'
+
+function Footer() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Footer
