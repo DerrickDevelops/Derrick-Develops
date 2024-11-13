f062b2d9f14500d764a039125f8be2473d090aa6
DerrickDevelops <derrickdevelops@gmail.com>
2024-11-13 18:29:52 -0500

    Minor Updates
    Added Content section & minor changes to h1 element in header.

---
+++ derrick-develops-portfolio/src/Content.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+import './Layout.css'
+
+function Content() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Content
--- derrick-develops-portfolio/src/Header.jsx
+++ derrick-develops-portfolio/src/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <>
       <div className='main'>
-        <h1>Good Morning</h1>
+        <h1>Good<br />Morning</h1>
         <div className='cards'>
           <div className='info-card'></div>
           <div className='id-card'></div>
