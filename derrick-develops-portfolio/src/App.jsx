79d34f6bfa62195b581052616cf09ba3be73383b
DerrickDevelops <derrickdevelops@gmail.com>
2024-12-16 17:38:01 -0500

    Added Nav
    Created nav.jsx & nav.css file.

--- derrick-develops-portfolio/src/App.jsx
+++ derrick-develops-portfolio/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header.jsx'
 import Content from './Content.jsx'
 import Footer from './Footer.jsx'
+import Nav from './Nav.jsx'
 import './layout.css';
 
 function App() {
   return (
     <div className="main">
+    <Nav />
     <Header />
     <Content />
     <Footer />
---
+++ derrick-develops-portfolio/src/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+import './Layout.css'
+
+function Nav() {
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Nav
